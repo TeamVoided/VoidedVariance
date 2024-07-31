32b0b2bac1e8b3919939dbde8bee0311f5b9112e
theendercore <theendercore@gmail.com>
2024-07-31 19:16:39 +0300

    - yippe dev time

--- src/main/kotlin/org/teamvoided/voided_variance/utils/Helpers.kt
+++ src/main/kotlin/org/teamvoided/voided_variance/utils/Helpers.kt
@@ -5,5 +5,7 @@ import net.minecraft.registry.Registry
 import net.minecraft.util.Identifier
 
 
-fun <T> Registry<T>.registerHolder(id: Identifier, entry:T): Holder.Reference<T> = Registry.registerHolder(this, id, entry)
-fun <T> Registry<T>.register(id: Identifier, entry:T): T = Registry.register(this, id, entry)
+fun <T> Registry<T>.registerHolder(id: Identifier, entry: T): Holder.Reference<T> =
+    Registry.registerHolder(this, id, entry)
+
+fun <T> Registry<T>.register(id: Identifier, entry: T): T = Registry.register(this, id, entry)
