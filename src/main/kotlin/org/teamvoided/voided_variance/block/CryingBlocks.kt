1f4a275bffc0d50ab536a5ab57d6d02d11994277
TheEnderCore <theendercore@gmail.com>
2026-08-21 18:51:50 +0300

    .

--- src/main/kotlin/org/teamvoided/voided_variance/block/CryingBlocks.kt
+++ src/main/kotlin/org/teamvoided/voided_variance/block/CryingBlocks.kt
@@ -1,6 +1,7 @@
 package org.teamvoided.voided_variance.block
 
-import net.minecraft.block.*
+import net.minecraft.block.Block
+import net.minecraft.block.BlockState
 import net.minecraft.particle.ParticleTypes
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
@@ -10,24 +11,24 @@ import net.minecraft.world.World
 interface CryingBlock {
     fun particle(state: BlockState, world: World, pos: BlockPos, random: RandomGenerator) {
         if (random.nextInt(5) == 0) {
-        val direction = Direction.random(random)
-        if (direction != Direction.UP) {
-            val blockPos = pos.offset(direction)
-            val blockState = world.getBlockState(blockPos)
-            if ( !state.isOpaque || !blockState.isSideSolidFullSquare(world, blockPos, direction.opposite)) {
-                val d =
-                    if (direction.offsetX == 0) random.nextDouble() else 0.5 + direction.offsetX.toDouble() * 0.6
-                val e =
-                    if (direction.offsetY == 0) random.nextDouble() else 0.5 + direction.offsetY.toDouble() * 0.6
-                val f =
-                    if (direction.offsetZ == 0) random.nextDouble() else 0.5 + direction.offsetZ.toDouble() * 0.6
-                world.addParticle(
-                    ParticleTypes.DRIPPING_OBSIDIAN_TEAR,
-                    pos.x.toDouble() + d, pos.y.toDouble() + e, pos.z.toDouble() + f,
-                    0.0, 0.0, 0.0
-                )
+            val direction = Direction.random(random)
+            if (direction != Direction.UP) {
+                val blockPos = pos.offset(direction)
+                val blockState = world.getBlockState(blockPos)
+                if (!state.isOpaque || !blockState.isSideSolidFullSquare(world, blockPos, direction.opposite)) {
+                    val d =
+                        if (direction.offsetX == 0) random.nextDouble() else 0.5 + direction.offsetX.toDouble() * 0.6
+                    val e =
+                        if (direction.offsetY == 0) random.nextDouble() else 0.5 + direction.offsetY.toDouble() * 0.6
+                    val f =
+                        if (direction.offsetZ == 0) random.nextDouble() else 0.5 + direction.offsetZ.toDouble() * 0.6
+                    world.addParticle(
+                        ParticleTypes.DRIPPING_OBSIDIAN_TEAR,
+                        pos.x.toDouble() + d, pos.y.toDouble() + e, pos.z.toDouble() + f,
+                        0.0, 0.0, 0.0
+                    )
+                }
             }
-        }
         }
     }
 }
